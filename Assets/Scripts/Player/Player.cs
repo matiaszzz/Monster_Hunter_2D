acd6ef80cf2ff2ec8d21f2d13ef7454d0ac632e4
matiaszzz <165030231+matiaszzz@users.noreply.github.com>
2025-11-11 03:45:40 -0300

    feat: Al player se le agrego movimiento y al background un tilemap

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5.0f;
+    private Rigidbody2D rb;
+    private float move;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(move * speed, rb.linearVelocity.y);
+    }
+}
