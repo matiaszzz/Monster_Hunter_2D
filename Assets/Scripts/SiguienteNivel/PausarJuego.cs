b4af47de42889b9c950ad6b89f5912b83c722213
matiaszzz <165030231+matiaszzz@users.noreply.github.com>
2025-11-28 14:38:43 -0300

    update

---
+++ Assets/Scripts/SiguienteNivel/PausarJuego.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class PausarJuego : MonoBehaviour
+{
+    public GameObject menuPausa;
+    public bool juegoPausado = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (juegoPausado)
+            {
+                Reanudar();
+            }
+            else
+            {
+                Pausar();
+            }
+        }
+    }
+
+    public void Reanudar()
+    {
+        menuPausa.SetActive(false);
+        Time.timeScale = 1;
+        juegoPausado = false;
+    }
+
+    public void Pausar()
+    {
+        menuPausa.SetActive (true);
+        Time.timeScale = 0;
+        juegoPausado = true;
+    }
+}
